bf91b19f548230c92277804d36bacfc373fa9a10
mirasac <50930731+mirasac@users.noreply.github.com>
2022-10-17 00:18:37 +0200

    Changed typos

--- 2022-10-14/integral_sin.cpp
+++ 2022-10-14/integral_sin.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
-#include <cstdio>
-#include "../mclib.h"
+#include <fstream>
+#include <ios>
+#include "../mclib/mclib.h"
 
 #define NUM_INTERVALS 4
+#define PRECISION 4
 
 /*
 Mathematical function I can change without editing the code for the quadrature.
@@ -18,9 +20,9 @@ double function(double x) {
 		_return = 1.0;
 	}
 	*/
-	// To avoid the singular point it is convenient sing the Taylor expansion up to an order of t where the machine epsilon is negligeble.
-	if (t < 1e-3) {
-		_return = 1.0 - t*t / (3.0 * 2) + t*t*t*t / 125.0;
+	// To avoid the singular point it is convenient sing the Taylor expansion up to an order of t where the machine epsilon is negligible.
+	if (x < 1e-3) {
+		_return = 1.0 - x*x / (3.0 * 2) + x*x*x*x / 125.0;
 	} else {
 		_return = sin(x) / x;
 	}
@@ -46,13 +48,17 @@ int main() {
 	double h[NUM_INTERVALS];
 	x = 0.8;
 	x_0 = 0.0;
-	FILE * out = fopen("out.dat", "a");
-	fputs("h, M(h), T(h)\n", out);
+	ofstream outfile;
+	outfile.open("out.dat");
+	outfile.precision(PRECISION);
+	outfile.setf(ios::scientific);
+	//outfile << "h, M(h), T(h)" << endl;
 	for (int n = 0; n < NUM_INTERVALS; n++) {
 		m = primitive(midpointquad, function, x, x_0, h[n]);
 		t = primitive(trapezioidalquad, function, x, x_0, h[n]);
-		fprintf(out, "%f, %f, %f\n", h[n], m, t);
+		//outfile << h[n] << ", " << m << ", " << t << endl;
 	}
+	outfile.close();
 	return 0;
 }
 
