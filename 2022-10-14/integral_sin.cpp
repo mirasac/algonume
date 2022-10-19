36514a6cfb7a04e379a418c97d461c79deaf4f15
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-10-19 15:05:44 +0200

    Exercise integral_sin.cpp now is working
    Created POSIX shell script to open gnuplot and plot data from multiple columns same file

--- 2022-10-14/integral_sin.cpp
+++ 2022-10-14/integral_sin.cpp
@@ -3,10 +3,12 @@
 #include <cstdlib>
 #include <fstream>
 #include <ios>
+#include <iomanip>
 #include "../mclib/mclib.h"
 
 #define NUM_INTERVALS 4
-#define PRECISION 4
+#define PRECISION 8
+#define COL_WIDTH 10
 
 /*
 Mathematical function I can change without editing the code for the quadrature.
@@ -44,19 +46,58 @@ double primitive(
 
 int main() {
 	using namespace std;
-	double m, t, x, x_0;
-	double h[NUM_INTERVALS];
+	double m, t, x, x_0, h, a, b, delta_x, delta_h;
+	double h_interval[NUM_INTERVALS], sum_prev[NUM_INTERVALS];
+	int num_x;
+	
+	// Datafile with integrals at various interval divisions.
 	x = 0.8;
 	x_0 = 0.0;
 	ofstream outfile;
-	outfile.open("out.dat");
+	outfile.open("outfile.dat");
 	outfile.precision(PRECISION);
-	outfile.setf(ios::scientific);
-	outfile << "h, M(h), T(h)" << endl;
+	//outfile.setf(ios::scientific);
+	outfile << setw(COL_WIDTH) << "h " << "M(h) " << "T(h)" << endl;
 	for (int n = 0; n < NUM_INTERVALS; n++) {
-		m = primitive(midpointquad, function, x, x_0, h[n]);
-		t = primitive(trapezioidalquad, function, x, x_0, h[n]);
-		outfile << h[n] << ", " << m << ", " << t << endl;
+		h = (x - x_0) / (1 << n);
+		m = primitive(midpointquad, function, x, x_0, h);
+		t = primitive(trapezioidalquad, function, x, x_0, h);
+		outfile << setw(COL_WIDTH) << h << " " << m << " " << t << endl;
+	}
+	outfile.close();
+	
+	// Datafile for plot of sin(x) / x with various interval divisions.
+	// Input data.
+	a = 0.0;
+	b = 25.0;
+	num_x = 1000;
+	// Output file preparation.
+	ofstream plotfile;
+	plotfile.open("plotfile.dat");
+	plotfile.precision(PRECISION);
+	plotfile << setw(COL_WIDTH) << "#x";
+	// Print number of intervals in the header.
+	for (int n = 0; n < NUM_INTERVALS; n++) {
+		plotfile << setw(COL_WIDTH) << " " << n+1;
+		sum_prev[n] = 0.0;
+	}
+	plotfile << endl;
+	// Integral evaluation.
+	orderinterval(&a, &b);
+	delta_x = (b - a) / num_x;
+	x_0 = a;
+	for (int i = 1; i <= num_x; i++) {
+		x = a + delta_x * i;
+		delta_h = x - x_0;
+		// MC mettere setw(COL_WIDTH) dove serve.
+		plotfile << x;
+		for (int n = 0; n < NUM_INTERVALS; n++) {
+			h = delta_h / (1 << n);
+			sum_prev[n] += primitive(trapezioidalquad, function, x, x_0, h);
+			plotfile << " " << sum_prev[n];
+		}
+		plotfile << endl;
+		x_0 = x;
 	}
 	outfile.close();
 	return 0;
