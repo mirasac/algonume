c11f086f64fee7de616a4e281cd90e83f5f74fbf
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-10-21 13:33:36 +0200

    Add other root finders
    Check if everything is working

--- mclib/mclib.h
+++ mclib/mclib.h
@@ -37,6 +37,25 @@ Value 1 if the theorem is valid for the given parameters, 0 otherwise.
 */
 char check_intermediate_value(double (*f)(double x), double a, double b);
 
+/*
+Evaluate the polynomial of order n defined by coefficients c in point x.
+
+Parameters
+----------
+n : int
+	Order of the polynomial.
+c : double[]
+	Array of coefficients, coefficient in position i is multiplied to x^i.
+x : double
+	Point where the polynomial is evaluated.
+*/
+double polynomial(int n, double c[], double x);
+
+/*
+Set array c1 as the array of coefficients of the derivative of the polynomial of coefficients c.
+*/
+void polynomial_derivative(int n, double c[], double c1[]);
+
 
 
 ////////// Quadrature functions //////////
@@ -96,3 +115,35 @@ Return
 The first point x where the function assumes a value lower than the specified tollerance.
 */
 double falseposition(double (*f)(double x), double a, double b, double tollerance);
+
+/*
+Find a zero of function f in the interval [a, b] within the specified tollerance using the secant method.
+
+Return
+------
+The first point x where the function assumes a value lower than the specified tollerance.
+*/
+double secant(double (*f)(double x), double a, double b, double tollerance);
+
+/*
+Find a zero of function f in the interval [a, b] within the specified tollerance using the Newton-Raphson method.
+
+Return
+------
+The first point x where the function assumes a value lower than the specified tollerance.
+*/
+double newtonraphson(double (*f)(double x), double (*f1)(double x), double a, double b, double tollerance);
+
+/*
+Variant of function newtonraphson to be used when function f is a polynomial.
+*/
+double newtonraphson_poly(double (*p)(int n, double c[], double x), int n, double c[], double a, double b, double tollerance);
+
+
+
+////////// Bracketing //////////
+
+
+
+int bracketing(double a, double b, int N, double x_l[], double x_r[]);
+
