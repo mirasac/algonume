60273fff837a9f28c2f870eccee1adbc35f160a1
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-10-21 13:32:17 +0200

    New lesson 2022-10-21
    Add other root finders
    Add polynomial function

--- 2022-10-20/froot.cpp
+++ 2022-10-20/froot.cpp
@@ -1,8 +1,18 @@
 #include <cmath>
 #include "../mclib/mclib.h"
 
+#define ORDER 4
+
 double function(double x) {
 	return exp(-x) - x;
+}
+
+double function1(double x) {
+	return -exp(-x) - 1.0;
+}
+
+double function2(double x) {
+	return exp(1.0 / (x + 0.5)) - (3.0 + 2.0 * x) / (1.0 + x);
 }
 
 int main() {
@@ -11,4 +21,24 @@ int main() {
 	b = 1.0;
 	tollerance = 1e-7;
 	bisection(function, a, b, tollerance);
+	secant(function, a, b, tollerance);
+	newtonraphson(function, function1, a, b, tollerance);
+	
+	// Practice session #3
+	double c[ORDER];
+	a = -5.0;
+	b = 0.0;
+	tollerance = 1e-8;
+	c[3] = 1.0;
+	c[2] = -3.0;
+	c[1] = 1.0;
+	c[0] = 5.0;
+	newtonraphson_poly(polynomial, ORDER, c, a, b, tollerance);
+	
+	// Practice session #4
+	a = 0.0;
+	b = 2.0;
+	tollerance = 1e-7;
+	secant(function2, a, b, tollerance);
 }
+
