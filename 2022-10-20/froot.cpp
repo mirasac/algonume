5f3123bca904256732d8fb5fd7fab84b5bdef8d2
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-10-27 13:34:23 +0200

    Add practice session #5

--- 2022-10-20/froot.cpp
+++ 2022-10-20/froot.cpp
@@ -2,17 +2,26 @@
 #include "../mclib/mclib.h"
 
 #define ORDER 4
+#define NUM_INTERVALS_5 10
 
 double function(double x) {
 	return exp(-x) - x;
 }
 
-double function1(double x) {
+double function_1(double x) {
 	return -exp(-x) - 1.0;
 }
 
 double function2(double x) {
 	return exp(1.0 / (x + 0.5)) - (3.0 + 2.0 * x) / (1.0 + x);
+}
+
+double function2_1(double x) {
+	return - x / ( (x + 0.5) * (x + 0.5) ) * exp(1.0 / (x + 0.5)) + 1 / (1 + x*x);
+}
+
+double function3(double x) {
+	return sin(x) - ( (x / 10.0) * (x / 10.0) + x / 5.0 + 1.0 / 3.0 );
 }
 
 int main() {
@@ -28,7 +37,7 @@ int main() {
 
 	cout << endl << "Practice session #2" << endl;
 	secant(function, a, b, tollerance);
-	newtonraphson(function, function1, a, b, tollerance);
+	newtonraphson(function, function_1, a, b, tollerance);
 	
 	cout << endl << "Practice session #3" << endl;
 	double c[ORDER];
@@ -45,6 +54,20 @@ int main() {
 	a = 0.0;
 	b = 2.0;
 	tollerance = 1e-7;
+	bisection(function2, a, b, tollerance);
+	falseposition(function2, a, b, tollerance);
 	secant(function2, a, b, tollerance);
+	newtonraphson(function2, function2_1, a, b, tollerance);
+	
+	cout << endl << "Practice session #5" << endl;
+	a = -10.0;
+	b = 10.0;
+	double dx;
+	double x_L[NUM_INTERVALS_5], x_R[NUM_INTERVALS_5];
+	int N;
+	N = bracketing(function3, a, b, NUM_INTERVALS_5, x_L, x_R);
+	for (int n = 0; n < N; n++) {
+		cout << "Root #" << n + 1 << "(bisection): " << bisection(function3, x_L[n], x_R[n], tollerance) << endl;
+	}
 }
 
