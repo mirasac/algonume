013c6eb71df7acf8b992eeac050cf4ad68a768af
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-10-27 13:34:47 +0200

    New lesson

---
+++ 2022-10-27/legendre.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include "../mclib/mclib.h"
+
+#define NUM_INTERVALS 100
+
+double eval_legendre(double x) {
+	int order;
+	order = 13;
+	return polynomial_legendre(order, x);
+}
+
+int main() {
+	using namespace std;
+	cout << setprecision(N_PRECISION);
+	double a, b, tollerance;
+	double x_L[NUM_INTERVALS], x_R[NUM_INTERVALS];
+	int N;
+	a = -1.0;
+	b = 1.0;
+	tollerance = 1e-15;
+	N = bracketing(eval_legendre, a, b, NUM_INTERVALS, x_L, x_R);
+	for (int n = 0; n < N; n++) {
+		cout << "Root #" << n + 1 << ": " << bisection(eval_legendre, x_L[n], x_R[n], tollerance) << endl;
+	}
+	return 0;
+}
+
