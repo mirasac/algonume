5c0f19d96439391728b448f4fdacef46dc32da6b
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-11-24 11:03:06 +0100

    Partial changes for poisson.cpp

--- 2022-11-17/poisson.cpp
+++ 2022-11-17/poisson.cpp
@@ -6,7 +6,11 @@
 
 void rhs(double r, double const Y_0[], double R[]) {
 	R[0] = Y_0[1];
-	R[1] = 0.5 * r * exp(-r);
+	R[1] = -0.5 * r * exp(-r);
+}
+
+double solution(double r) {
+	return 1.0 - 0.5 * (r + 2) * exp(-r);
 }
 
 int main() {
@@ -19,26 +23,42 @@ int main() {
 	double const s_step = 0.1;
 	int const N = 1000;
 	int const n_eq = 2;
-	int const n_s = 0;  // MC just for test.
-	double s, r_min, r_max, r, dr;
-	double Y[n_eq];
+	double r_min, r_max, r, dr;
+	double Y_1[n_eq], Y_2[n_eq], Y_3[n_eq], Y_4[n_eq], Y_5[n_eq], Y_6[n_eq];
 	r_min = 0.0;
 	r_max = b;
 	dr = (r_max - r_min) / N;
+	Y_1[0] = 0.0;
+	Y_1[1] = 0.0;
+	Y_2[0] = 0.0;
+	Y_2[1] = 0.2;
+	Y_3[0] = 0.0;
+	Y_3[1] = 0.4;
+	Y_4[0] = 0.0;
+	Y_4[1] = 0.6;
+	Y_5[0] = 0.0;
+	Y_5[1] = 0.8;
+	Y_6[0] = 0.0;
+	Y_6[1] = 1.0;
 	plot_file.open("poisson_1.dat");
 	plot_file << setprecision(N_PRECISION) << scientific;
 	plot_file << "r phi(r,s=0.0) phi(r,s=0.2) phi(r,s=0.4) phi(r,s=0.6) phi(r,s=0.8) phi(r,s=1.0)" << endl;
-	// Integrate at different values of s.
-	for (int i_s = 0; i_s <= n_s; i_s++) {
-		s =  (2 * i_s) * s_step;
-		Y[0] = 0.0;
-		Y[1] = s;
-		for (int i_r = 0; i_r <= N; i_r++) {
-			r = r_min + i_r * dr;
-			plot_file << r << ' ' << Y[0];
-			rungekutta4(r, dr, Y, rhs, n_eq);
-			plot_file << endl;
-		}
+	for (int i_r = 0; i_r <= N; i_r++) {
+		r = r_min + i_r * dr;
+		plot_file << r;
+		plot_file << ' ' << Y_1[0];
+		rungekutta4(r, dr, Y_1, rhs, n_eq);
+		plot_file << ' ' << Y_2[0];
+		rungekutta4(r, dr, Y_2, rhs, n_eq);
+		plot_file << ' ' << Y_3[0];
+		rungekutta4(r, dr, Y_3, rhs, n_eq);
+		plot_file << ' ' << Y_4[0];
+		rungekutta4(r, dr, Y_4, rhs, n_eq);
+		plot_file << ' ' << Y_5[0];
+		rungekutta4(r, dr, Y_5, rhs, n_eq);
+		plot_file << ' ' << Y_6[0];
+		rungekutta4(r, dr, Y_6, rhs, n_eq);
+		plot_file << endl;
 	}
 	plot_file.close();
 	
@@ -47,6 +67,14 @@ int main() {
 	// Point 3.
 	
 	// Point 4.
+	plot_file.open("poisson_4.dat");
+	plot_file << setprecision(N_PRECISION) << scientific;
+	plot_file << "r phi(r)" << endl;
+	for (int i_r = 0; i_r <= N; i_r++) {
+		r = r_min + i_r * dr;
+		plot_file << r << ' ' << solution(r) << endl;
+	}
+	plot_file.close();
 	return 0;
 }
 
