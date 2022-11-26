d21f915dc0089b96f4d7ec5db3fa23f247d3f30c
mirasac <50930731+mirasac@users.noreply.github.com>
2022-11-26 01:31:32 +0100

    Practice session wave.cpp completed

--- 2022-11-17/wave.cpp
+++ 2022-11-17/wave.cpp
@@ -21,7 +21,7 @@ double residual(double k) {
 	dx = (x_max - x_min) / N;
 	Y[0] = 0.0;
 	Y[1] = 1.0;  // This is s.
-	global_k = k;  // MC probabilmente non funziona, passarlo come terzo valore a Y.
+	global_k = k;
 	for (int n = 0; n <= N; n++) {
 		x = x_min + n * dx;
 		rungekutta4(x, dx, Y, rhs, n_eq);
@@ -31,7 +31,7 @@ double residual(double k) {
 
 int main() {
 	using namespace std;
-	cout << setprecision(N_PRECISION);
+	cout << setprecision(N_PRECISION) << scientific;
 	int const N = 100;
 	int const n_eq = 2;
 	double x_min, x_max, x, dx, s;
@@ -43,6 +43,7 @@ int main() {
 	s = 1.0;
 	
 	// Point 1.
+	cout << "Point 1" << endl;
 	global_k = 1.0;
 	Y[0] = 0.0;
 	Y[1] = s;
@@ -55,8 +56,10 @@ int main() {
 		rungekutta4(x, dx, Y, rhs, n_eq);
 	}
 	plot_file.close();
+	cout << "Plot generated" << endl;
 	
 	// Point 2.
+	cout << "\nPoint 2" << endl;
 	plot_file.open("wave_2.dat");
 	plot_file << setprecision(N_PRECISION) << scientific;
 	plot_file << "x phi(x)" << endl;
@@ -72,25 +75,26 @@ int main() {
 		plot_file << '\n' << endl;
 	}
 	plot_file.close();
+	cout << "Plot generated" << endl;
 	
 	// Point 3.
 	cout << "\nPoint 3" << endl;
-	double tollerance;
+	double k_min, k_max, tollerance;
 	tollerance = 1e-6;
-	x_min = 3.0;
-	x_max = 4.0;
-	cout << bisection(residual, x_min, x_max, tollerance) << endl;
+	k_min = 3.0;  // Found graphically
+	k_max = 4.0;  // Found graphically.
+	cout << bisection(residual, k_min, k_max, tollerance) << endl;
 	
 	// Point 4.
 	cout << "\nPoint 4" << endl;
-	x_min = 0.0;
-	x_max = 20.0;
 	int const n_brack_max = 8;
 	int n_brack;
-	double x_L[n_brack], x_R[n_brack];
-	n_brack = bracketing(residual, x_min, x_max, n_brack_max, x_L, x_R);
+	double k_L[n_brack], k_R[n_brack];
+	k_min = 0.0;
+	k_max = 20.0;
+	n_brack = bracketing(residual, k_min, k_max, n_brack_max, k_L, k_R);
 	for (int n = 0; n < n_brack; n++) {
-		cout << bisection(residual, x_L[n], x_R[n], tollerance) << endl;
+		cout << bisection(residual, k_L[n], k_R[n], tollerance) << endl;
 	}
 	return 0;
 }
