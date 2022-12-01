7affcc25726e70b21b0d01f8d7148ce272fffeee
Marco Casari <50930731+mirasac@users.noreply.github.com>
2022-12-01 12:41:27 +0100

    Implemented tridiagonal solver
    Completed practice session tridiag.cpp

--- 2022-11-25/tridiag.cpp
+++ 2022-11-25/tridiag.cpp
@@ -8,7 +8,12 @@ int main() {
 	using namespace std;
 	cout << fixed << setprecision(N_PRECISION);
 	int const N = 5;
-	double ** A;
+	double * x;
+	double d_inf[N] = {0.0, 1.0, 1.0, 1.0, 1.0};
+	double d[N] = {2.0, 2.0, 2.0, 2.0, 2.0};
+	double d_sup[N] = {1.0, 1.0, 1.0, 1.0, 0.0};
+	double b[N] = {1.0, 0.0, 3.0, 1.0, 0.0};
+	/*double ** A;
 	double b[N] = {1, 0, 3, 1, 0};
 	A = mat_new(N, N);
 	mat_zero(A, N, N);
@@ -24,10 +29,14 @@ int main() {
 	A[0][1] = 1;
 	A[1][2] = 1;
 	A[2][3] = 1;
-	A[3][4] = 1;
+	A[3][4] = 1;*/
 	cout << "Expected x = (2, -3, 4, -2, 1)" << endl;
-	cout << "Result x =" << endl;
-	// MC print result.
+	cout << "Result x is:" << endl;
+	x = tridiagonal_solver(d_inf, d, d_sup, b, N);
+	for (int i = 0; i < N; i++) {
+		cout << x[i] << endl;
+	}
+	delete[] x;
 	return 0;
 }
 
