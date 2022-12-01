df024eb9dfffaa5da9d09a6c49fc1b189c5dfa91
mirasac <50930731+mirasac@users.noreply.github.com>
2022-12-01 21:51:51 +0100

    bvp.cpp completed
    Deleted useless files from previous emergency commit

--- 2022-11-25/bvp.cpp
+++ 2022-11-25/bvp.cpp
@@ -4,46 +4,41 @@
 #include <iostream>
 #include "../mclib/mclib.h"
 
-double rhs(double x) {
-	return 1.0;
-}
-
 int main() {
 	using namespace std;
 	cout << fixed << setprecision(N_PRECISION);
-	int const N = 30;
-	double const y_lower = 0.0;  // Boundary condition y(0) = 0.
-	double const y_upper = 0.0;  // Boundary condition y(1) = 0.
+	int const N = 32;  // Points, two of which are boundaries.
 	double const x_min = 0.0;
 	double const x_max = 1.0;
+	double const y_lower = 0.0;  // Boundary condition y(0) = 0.
+	double const y_upper = 0.0;  // Boundary condition y(1) = 0.
 	double dx, x;
 	double d_inf[N], d[N], d_sup[N], b[N], * y;
-	// Assign values.
-	dx = (x_max - x_min) / N;
+	dx = (x_max - x_min) / (N - 1);
+	// Initialize arrays.
 	for (int i = 0; i < N; i++) {
-		x = x_min + i * dx;
 		d_inf[i] = 1.0;
 		d[i] = -2.0;
 		d_sup[i] = 1.0;
-		b[i] = dx*dx * rhs(x);
+		b[i] = dx*dx;  // RHS is set to 1.
 	}
-	b[0] -= y_lower;
-	b[N-1] -= y_upper;
+	b[1] -= y_lower;
+	b[N-2] -= y_upper;
 	// Open data file.
 	ofstream plot_file;
 	plot_file.open("bvp.dat");
 	plot_file << "x y(x)" << endl;
 	// Solve using tridiagonal matrix.
-	y = tridiagonal_solver(d_inf, d, d_sup, b, N);
-	plot_file << x << ' ' << y_lower;
-	for (int i = 0; i < N; i++) {
+	y = tridiagonal_solver(d_inf+1, d+1, d_sup+1, b+1, N-2);
+	cout << y[0] << ' ' << y[N-1] << endl;
+	plot_file << x_min << ' ' << y_lower << endl;
+	for (int i = 0; i <= N-3; i++) {
 		x = x_min + (i + 1) * dx;
 		plot_file << x << ' ' << y[i] << endl;
 	}
-	plot_file << x << ' ' << y_upper;
+	plot_file << x_max << ' ' << y_upper << endl;
 	// Clean up.
 	plot_file.close();
 	delete[] y;
 	return 0;
 }
-
