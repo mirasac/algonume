12f9d4b8d35fc13f8e10f9cbeae27c81f20516a7
mirasac <50930731+mirasac@users.noreply.github.com>
2023-08-03 18:15:59 +0200

    Start adding support code for absorbers
    Move to dynamic allocation of memory for arrays.

--- project/code/main.cpp
+++ project/code/main.cpp
@@ -7,28 +7,54 @@
 #include "../../mclib/mclib.h"
 #include "radiation.h"
 
-void set_layers_z_uniform(double z_TOA, int n_layers, double z[], double dz[]);
+void set_layers_z_uniform(double z_TOA, int n_layers, double z[], double delta_z[]);
 
 int main(int argc, char * argv[]) {
 	using namespace std;
 	cout << setprecision(N_PRECISION);
 	
 	// Configure atmospheric layers.
-	int const n_layers = 20;
+	int n_layers = 20;
 	double z_TOA, z_S; // [m]
-	double z[n_layers], dz[n_layers]; // [m]
+	double * z, * delta_z; // [m]
 	z_TOA = 42000.0;
 	z_S = 0;
-	set_layers_z_uniform(z_TOA, n_layers, z, dz);
+	z = new double[n_layers];
+	delta_z = new double[n_layers];
+	set_layers_z_uniform(z_TOA, n_layers, z, delta_z);
+	
+	// Configure absorbers.
+	absorber_t O3, CO2, soot;
+	// MC continue with detailing the optical bandwidths of absorbers.
+	
+	int n_absorbers[n_layers];
+	absorber_t ** absorbers;
+	absorbers = new absorber_t*[n_layers];
+	for (int i_layers = 0; i_layers < n_layers; i_layers++) {
+		cout << z[i_layers] << endl; // MC debug;
+		n_absorbers[i_layers] = 2;
+		absorbers[i_layers] = new absorber_t[n_absorbers[i_layers]];
+		for (int i_a = 0; i_a < n_absorbers[i_layers]; i_a++) {
+			absorbers[i_layers][i_a] = CO2;
+		}
+	}
+	
+	// Clean up.
+	delete[] z;
+	delete[] delta_z;
+	for (int i = 0; i < n_layers; i++) {
+		delete[] absorbers[i];
+	}
+	delete[] absorbers;
 	
 	return 0;
 }
 
-void set_layers_z_uniform(double z_TOA, int n_layers, double z[], double dz[]) {
-	dz[0] = z_TOA / n_layers;
-	z[0] = z_TOA - dz[0] / 2.0;
+void set_layers_z_uniform(double z_TOA, int n_layers, double z[], double delta_z[]) {
+	delta_z[0] = z_TOA / n_layers;
+	z[0] = z_TOA - delta_z[0] / 2.0;
 	for (int i_z = 1; i_z < n_layers; i_z++) {
-		dz[i_z] = dz[0];
-		z[i_z] = z[i_z-1] - dz[i_z];
+		delta_z[i_z] = delta_z[0];
+		z[i_z] = z[0] - delta_z[0] * i_z;
 	}
 }
--- project/code/radiation.h
+++ project/code/radiation.h
@@ -4,6 +4,12 @@
 #include "constants.h"
 #include "functions.h"
 
+struct absorber_t {
+	int n_nu;
+	double * nu; // [1 / mm]
+	double * delta_nu; // [1 / mm]
+};
+
 double total_flux_longwave(double T, int n_nu, double nu[], double dnu[]);
 
 double flux_longwave(double T, double nu, double dnu);
