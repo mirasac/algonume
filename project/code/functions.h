1596d275794232f0d65f23c695b4a3d9c1c325e0
mirasac <50930731+mirasac@users.noreply.github.com>
2023-08-05 12:14:21 +0200

    Add description to Planck's function

--- project/code/functions.h
+++ project/code/functions.h
@@ -9,8 +9,14 @@
 #define GAUSS_QUAD_POINTS 4
 #define GAUSS_QUAD_INTERVALS 10
 
-double planck_law(double nu, double T);
+/*
+Evaluate the spectral radiance of a blackbody for wavenumber nu [1 / m] and temperature T [K].
+*/
+double planck_law_nu(double nu, double T);
 
-double planck_law_average(double nu, double dnu, double T);
+/*
+Evaluate the average net flux of a blackbody in the interval [nu, nu + dnu] with units [1 / m] and temperature T [K].
+*/
+double planck_law_nu_average(double nu, double dnu, double T);
 
 #endif /* FUNCTIONS_H */
