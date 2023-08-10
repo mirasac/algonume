22e3083d109a3b060f06c98b22c3180fe338801c
mirasac <50930731+mirasac@users.noreply.github.com>
2023-08-10 12:45:40 +0200

    Add table of constants and update values
    Fine-tune report makefile.

--- project/code/constants.h
+++ project/code/constants.h
@@ -1,12 +1,12 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-double const global_S = 1361; // [W / m^2]
+double const global_S_0 = 1361; // [W / m^2]
 double const global_alpha = 0.306;
 double const global_g = 9.80665; // [m / s^2]
 double const global_P_0 = 100000; // [Pa]
 double const global_T_0 = 288.15; // [K]
-double const global_R = 8.314462618; // [m^3 Pa / (K mol)]
+double const global_R = 8.31446261815324; // [m^3 Pa / (K mol)]
 double const global_z_0 = 0.0; // [m]
 double const global_Gamma_0 = 0.0065; // [K / m]
 int const global_nu_threshold = 250000; // [1 / m]
