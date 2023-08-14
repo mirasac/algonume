64f7c6c80a0081f2ec32c0da62ce1c3bb174aebf
mirasac <50930731+mirasac@users.noreply.github.com>
2023-08-14 17:59:13 +0200

    Add script to create and export gnuplot plots

---
+++ project/code/plot.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Global variables and functions definition.
+DEF_DIR_EXPORT='../report/figures'
+
+print_usage() {
+	cat <<-EOF
+		plot.sh
+		Create plot in a separate window using a specific gnuplot script.
+		
+		The first argument is mandatory and its value is used as basename for the gnuplot script.
+		If the second argument is specified, the plot is also exported to a file. If the second argument has value 'export', then the exported file has same basename of the gnuplot script, else it has basename equal to the specified value.
+		If the third argument is specified, the directory specified as value is used to save the exported file, else the default path '${DEF_DIR_EXPORT}' is used.
+	EOF
+}
+
+# Get arguments.
+basename="$1"
+if [ -z "${basename}" ]
+then
+	echo 'Error: the first argument is mandatory.'
+	print_usage
+else
+	basename_export="$2"
+	dir_export="${3:-${DEF_DIR_EXPORT}}"
+
+	# Main plot command.
+	command="load '${basename}.gp'"
+
+	# Add commands to export to file.
+	if [ -n "${basename_export}" ]
+	then
+		if [ "${basename_export}" = 'export' ]
+		then
+			basename_export="${basename}"
+		fi
+		command=$(
+		cat <<-EOF
+			${command}
+			set terminal pdfcairo
+			set output "${dir_export}/${basename_export}.pdf"
+			replot
+		EOF
+		)
+	fi
+
+	echo "${command}"
+
+	# Launch gnuplot.
+	$(
+	gnuplot --persist <<-EOF
+		${command}
+	EOF
+	)
+fi
