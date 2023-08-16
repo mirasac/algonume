76bb3b480b990c79b6edab2cbfd817bd25ad7a56
mirasac <50930731+mirasac@users.noreply.github.com>
2023-08-16 21:53:09 +0200

    Change plot script to lua tikz terminal

--- project/code/plot.sh
+++ project/code/plot.sh
@@ -3,6 +3,7 @@
 # Global variables and functions definition.
 FILENAME_SCRIPT='plot.sh'
 DEF_DIR_EXPORT='../report/figures'
+BASENAME_LUA_TIKZ='gnuplot-lua-tikz'
 
 print_info() {
 	echo "${FILENAME_SCRIPT} INFO: $1"
@@ -15,48 +16,67 @@ print_error() {
 print_usage() {
 	cat <<-EOF
 		${FILENAME_SCRIPT}
-		Create plot in a separate window using a specific gnuplot script.
+		Create plot using a specific gnuplot script.
 		
 		The first argument is mandatory and its value is used as basename for the gnuplot script.
-		If the second argument is specified, the plot is also exported to a file. If the second argument has value 'export', then the exported file has same basename of the gnuplot script, else it has basename equal to the specified value.
-		If the third argument is specified, the directory specified as value is used to save the exported file, else the default path '${DEF_DIR_EXPORT}' is used.
+		If the second argument is specified with value 'export', then the plot is also exported to a file with same basename of the gnuplot script. The default directory for the exported file is '${DEF_DIR_EXPORT}'.
+		If the third argument is specified, its value is used as directory for the exported file.
 	EOF
 }
 
-# Get arguments.
+
 basename="$1"
 if [ -z "${basename}" ]
 then
 	print_error 'the first argument is mandatory'
 	print_usage
 else
-	basename_export="$2"
-	dir_export="${3:-${DEF_DIR_EXPORT}}"
-
+	option="$2"
+	
 	# Main plot command.
 	command="load '${basename}.gp'"
-
+	
 	# Add export commands.
-	if [ -n "${basename_export}" ]
+	if [ "${option}" = 'export' ]
 	then
-		if [ "${basename_export}" = 'export' ]
-		then
-			basename_export="${basename}"
-		fi
+		dir_export="${3:-${DEF_DIR_EXPORT}}"
+		basename_export="${basename}"
 		command=$(
 		cat <<-EOF
 			${command}
-			set terminal pdfcairo
-			set output "${dir_export}/${basename_export}.pdf"
+			set terminal lua tikz latex color gparrows gppoints picenvironment latex createstyle
+			set output "${dir_export}/${basename_export}.tex"
 			replot
 		EOF
 		)
 	fi
-
+	
 	# Launch gnuplot.
 	$(
 	gnuplot --persist <<-EOF
 		${command}
 	EOF
 	)
+	
+	# Convert TikZ source to PDF figure.
+	if [ "${option}" = 'export' ]
+	then
+		filename_tmp_source="${dir_export}/${basename_export}.tmp"
+		# Create LaTeX source.
+		$(
+		cat <<-EOF > "${filename_tmp_source}"
+			\documentclass[tikz, border=0mm]{standalone}
+			\usepackage{${BASENAME_LUA_TIKZ}}
+			\begin{document}
+			\include{${basename_export}.tex}
+			\end{document}
+		EOF
+		)
+		# Convert LaTeX source to PDF figure.
+		pdflatex -cnf-line='main_memory = 10000000' -cnf-line='main_memory.pdflatex = 10000000' -output-directory="${dir_export}" "${filename_tmp_source}"
+		# Delete temporary files.
+		rm -f "${filename_tmp_source}*"
+		rm "${BASENAME_LUA_TIKZ}.tex"
+		rm "t-${BASENAME_LUA_TIKZ}.tex"
+	fi
 fi
