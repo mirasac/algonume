6f15b9e48e778cde428096bdebc82f385e46bbd8
mirasac <50930731+mirasac@users.noreply.github.com>
2023-08-17 11:15:41 +0200

    Add general Makefile
    Still in testing phase

--- project/data/plot.sh
+++ project/data/plot.sh
@@ -18,7 +18,7 @@ print_usage() {
 		Create plot using a specific gnuplot script.
 		
 		The first argument is mandatory and its value is used as basename for the gnuplot script.
-		If the second argument is specified with value 'export', then the plot is also exported to a file with same basename of the gnuplot script. The default directory for the exported file is '${DEF_DIR_EXPORT}'.
+		If the second argument is specified, if its value is 'export' then the plot is also exported to a file with same basename of the gnuplot script, if its value is 'exportonly', the plot is not displayed in a separate window and only the export is performed, useful in systems which lack of GUI support. The default directory for the exported file is '${DEF_DIR_EXPORT}'.
 		If the third argument is specified, its value is used as directory for the exported file.
 	EOF
 }
@@ -36,16 +36,32 @@ else
 	command="load '${basename}.gp'"
 	
 	# Add export commands.
-	if [ "${option}" = 'export' ]
+	if [ -n "${option}" ]
 	then
 		dir_export="${3:-${DEF_DIR_EXPORT}}"
 		basename_export="${basename}"
+		command_export=$(
+		cat <<-EOF
+			set terminal cairolatex pdf input colourtext noheader color size 14.85cm,10.5cm
+			set output '${dir_export}/${basename_export}.tex'
+		EOF
+		)
+	fi
+	if [ "${option}" = 'export' ]
+	then
 		command=$(
 		cat <<-EOF
 			${command}
-			set terminal cairolatex pdf input colourtext noheader color size 14.85cm,10.5cm
-			set output '${dir_export}/${basename_export}.tex'
+			${command_export}
 			replot
+		EOF
+		)
+	elif [ "${option}" = 'exportonly' ]
+	then
+		command=$(
+		cat <<-EOF
+			${command_export}
+			load '${basename}.gp'
 		EOF
 		)
 	fi
