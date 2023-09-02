a87b9c85b0727e75dbbf713d26091825810d729e
mirasac <50930731+mirasac@users.noreply.github.com>
2023-09-02 08:12:05 +0200

    Fix pressure for vertical coordinates

--- project/code/main.cpp
+++ project/code/main.cpp
@@ -84,9 +84,10 @@ int main(int argc, char * argv[]) {
 		T_TOA = T[0];
 		P_TOA = get_pressure(z[0], T_TOA);
 		for (int i_z = 0; i_z < n_layers; i_z++) {
+			P = get_pressure(z[i_z], T[i_z]);
 			file_plot << t << ' ' << z[i_z] << ' ' << T[i_z] << ' ' << P << ' ' << get_sigma(P, P_TOA) << ' ' << get_theta(T[i_z], P) << '\n';
 		}
-		file_plot << t << ' ' << global_z_g << ' ' << T[n_layers] << ' ' << P << ' ' << get_sigma(P, P_TOA) << ' ' << get_theta(T[n_layers], P) << '\n';
+		file_plot << t << ' ' << global_z_g << ' ' << T[n_layers] << ' ' << global_P_g << ' ' << get_sigma(global_P_g, P_TOA) << ' ' << get_theta(T[n_layers], global_P_g) << '\n';
 		eulerstep(t, dt, T, rhs, n_layers);
 		// MC an additional separate line of calculation is needed for values at ground level.
 		file_plot << '\n';
@@ -109,14 +110,12 @@ int main(int argc, char * argv[]) {
 
 void rhs(double t, double const * Y_0, double * R, int const n_eq) {
 	int i_0_z, i_0_param;
-	double P; // / Pa
 	double nu_div; // / (1 / m)
 	double E_L, E_S; // / (W / m^2)
 	i_0_z = n_eq + 1; // Index of the first value of altitude.
 	i_0_param = i_0_z + n_eq + 1; // Index of the first parameter.
 	nu_div = Y_0[i_0_param];
 	for (int i = 0; i < n_eq; i++) {
-		P = get_pressure(Y_0[i_0_z + i], Y_0[i]);
 		E_L = longwave_irradiance(global_nu_min, nu_div, 55, n_eq, Y_0, Y_0 + i_0_z); // MC change to n_nu = 21 to have dnu ~ 100 / cm.
 		E_S = shortwave_irradiance(nu_div, global_nu_max, 38, n_eq, Y_0, Y_0 + i_0_z); // MC change to n_nu = 978 to have dnu ~ 100 / cm.
 		R[i] = - (E_L + E_S) / (Y_0[i_0_z + i + 1] - Y_0[i_0_z + i])
