4fa48387a90c22c552d88c9ede31da2949cf68d5
mirasac <50930731+mirasac@users.noreply.github.com>
2023-09-05 06:46:54 +0200

    Complete description radiative equilibrium

--- project/code/configuration.h
+++ project/code/configuration.h
@@ -14,9 +14,11 @@
 #define QUAD_INTERVALS 100000
 
 // Arbitrary values.
+double const global_D = 1.66;
 int const global_N = 20;
 double const global_nu_min = 1e4; // 1 / m
 double const global_nu_max = 1e7; // 1 / m
+double const global_P_TOA = 100.0; // Pa
 double const global_z_TOA = 55000.0; // m
 
 #endif /* CONFIGURATION_H */
