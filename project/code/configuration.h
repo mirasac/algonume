a09efa2174dfaefa2d6288544ccfb29d2caf3695
mirasac <50930731+mirasac@users.noreply.github.com>
2023-09-05 14:05:22 +0200

    Organize files for minimal presentation

--- project/code/configuration.h
+++ project/code/configuration.h
@@ -15,13 +15,14 @@
 
 // Arbitrary values.
 double const global_delta_g = 0.8;
-double const global_D = 1.66;
-double const global_z_0 = 2000.0; // / m
 int const global_N = 100;
 double const global_nu_min = 1e4; // 1 / m
 double const global_nu_max = 1e7; // 1 / m
+double const global_P_0 = 1e5; // Pa
 double const global_P_TOA = 3.0; // Pa
-double const global_mu_m = global_delta_g * global_g / (global_P_g - global_P_TOA);
+double const global_z_0 = 2000.0; // / m
 double const global_z_TOA = 55000.0; // m
+
+double const global_mu_m = global_delta_g * global_g / (global_P_g - global_P_TOA);
 
 #endif /* CONFIGURATION_H */
